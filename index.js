79e3c9c7971955d3f22bfb9d2b392b9c804e4e65
naomi curapil <curapilnaomi@icloud.com>
2021-11-02 17:57:45 -0500

    commit tree 87f7d288c0ac5980dcf151e7d23757417d580763

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let messageDistance;
+    if (distance <= 400) {
+      messageDistance = 'This one is on me!';
+
+    }else if (distance > 2000 && distance < 2500) {
+      messageDistance = 'I will gladly take your thirty bucks.';
+    }else if (distance > 2500) {
+      messageDistance = 'No can do.';
+    }
+    return messageDistance;
+
+  
+  }
+
+function ternaryCheckCity(city){
+  const message = city === 'NYC' ? 'Ok, sounds good.': "No go.";
+  return message;
+
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tipResponse){
+ switch (tipResponse) {
+  case 'generous':
+    return 'Thank you so much.'
+  case 'not as generous':
+    return 'Thank you.'
+  default:
+    return 'Bye.'
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
